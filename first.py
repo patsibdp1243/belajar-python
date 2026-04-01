2c2c08e5dc65ef83fb66382138bf376088ead0ac
patsi <aalkoaludin@gmail.com>
2026-04-01 14:22:35 +0700

    ini projek git pertama isinya belajar python

---
+++ first.py
@@ -0,0 +1,20 @@
+principle = 0
+rate = 0
+time = 0
+
+while principle <= 0:
+    principle = float(input("Enter the principle amount : "))
+    if principle <= 0:
+        print("Principle cant be less or equal to zero")
+
+while rate <= 0:
+    rate = float(input("Enter the interest rate amount : "))
+    if rate <= 0:
+        print("Interest rate cant be less or equal to zero")
+
+while time <= 0:
+    time = float(input("Enter the time amount : "))
+    if time <= 0:
+        print("Time cant be less or equal to zero")
+total = principle * pow((1 + rate / 100), time)
+print(f"Balance after {time} year/s: ${total:.2f}")
---
+++ main.py
@@ -0,0 +1,37 @@
+"""import time
+
+my_time = int(input("Enter time in seconds : "))
+
+for x in range (my_time, 0, -1):
+    seconds = x % 60
+    minutes = int(x / 60) % 60
+    hours = int(x / 3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+
+print("Times up") """
+
+foods = []
+prices = []
+total = 0
+
+while True:
+    food = input("Enter a food to buy (q to quit) : ")
+    if food.lower() == "q":
+        break
+    else:
+        price = float(input(f"Enter the price of {food} : $"))
+        foods.append(food)
+        prices.append(price)
+
+
+print("----- YOUR CART -----")
+
+for food in foods:
+    print(food , end=" ")
+    
+print()
+for price in prices:
+    total += price
+
+print(f"Your total is : {total}")
